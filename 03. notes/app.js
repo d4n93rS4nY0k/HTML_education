3a733d59cd64029582681b9d20a0e2e2985bfabb
d4n93rs4ny0k <dangersanyokgit@gmail.com>
2025-07-23 22:30:12 +0300

    Last checkpoint 03. notes Massive and Objects

--- 03. notes/app.js
+++ 03. notes/app.js
@@ -2,32 +2,66 @@ const inputElement = document.getElementById('input')
 const addBtn = document.getElementById('add')
 const listElement = document.getElementById('list')
 
-const notes = ['Сходить в магазин', 'Убраться дома']
-console.log(notes.length)
+const doneBtn = document.getElementById('completedBtn')
+
+const notes = [
+    {
+        title: 'Сходить в магазин',
+        completed: false,
+    },
+    {
+        title: 'Выбросить мусор',
+        completed: false,
+    },
+]
 
 function render () {
-    for (i = 0; i < notes.length; i++){
-        console.log(i)
-        listElement.insertAdjacentHTML(
-            'beforeend', 
-            `
-                <div class="note">
-                    <p>${notes[i]}</p>
-
-                    <div class="action-buttons">
-                        <button class="green">✓</button>
-                        <button class="red">✖</button>
-                    </div>
-                </div>
-            `
-        )
+    for (let note of notes) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(note))
     }
+}
 
-    inputElement.value = ''
+function getNoteTemplate(note) {
+    if(note.completed === true){
+        return `
+            <div class="note completed-note">
+                <p>${note.title}</p>
+
+                <div class="action-buttons">
+                    <button id='completedBtn' class="completed">✓</button>
+                    <button id='deleteBtn' class="red">✖</button>
+                </div>
+            </div>
+        `
+    }
+    return `
+        <div class="note">
+            <p>${note.title}</p>
+
+            <div class="action-buttons">
+                <button id='completedBtn' class="green">✓</button>
+                <button id='deleteBtn' class="red">✖</button>
+            </div>
+        </div>
+    `
 }
 
 render()
 
 addBtn.onclick = function () {
-    
+    if(inputElement.value.length === 0){
+        return
+    }
+
+    const newNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(newNote))
+    inputElement.value = ''
+}
+
+doneBtn.onclick = function () {
+    notes[1].completed = true
 }
